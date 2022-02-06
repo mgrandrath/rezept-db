aba5f91c6ad8469c45a4b062b67e06a1d66377ea
Martin Grandrath <mail@martin-grandrath.de>
2022-02-06 14:18:58 +0100

    Add columns for recipe source to db schema

---
+++ server/prisma/migrations/20220206131817_add_recipe_source/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Recipe" ADD COLUMN "offlineSourceName" TEXT;
+ALTER TABLE "Recipe" ADD COLUMN "offlineSourcePage" TEXT;
+ALTER TABLE "Recipe" ADD COLUMN "onlineSourceUrl" TEXT;
+ALTER TABLE "Recipe" ADD COLUMN "sourceType" TEXT;
