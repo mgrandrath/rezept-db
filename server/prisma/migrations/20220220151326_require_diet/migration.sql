897268380267f4205871804f172ec015e450fc91
Martin Grandrath <mail@martin-grandrath.de>
2022-02-20 16:14:54 +0100

    Require diet attribute

---
+++ server/prisma/migrations/20220220151326_require_diet/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - Made the column `diet` on table `Recipe` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Recipe" (
+    "recipeId" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "sourceType" TEXT NOT NULL,
+    "onlineSourceUrl" TEXT,
+    "offlineSourceTitle" TEXT,
+    "offlineSourcePage" INTEGER,
+    "diet" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Recipe" ("createdAt", "diet", "name", "notes", "offlineSourcePage", "offlineSourceTitle", "onlineSourceUrl", "recipeId", "sourceType", "updatedAt") SELECT "createdAt", "diet", "name", "notes", "offlineSourcePage", "offlineSourceTitle", "onlineSourceUrl", "recipeId", "sourceType", "updatedAt" FROM "Recipe";
+DROP TABLE "Recipe";
+ALTER TABLE "new_Recipe" RENAME TO "Recipe";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
