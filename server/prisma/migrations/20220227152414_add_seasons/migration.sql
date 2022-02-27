3e296773459c79cf67fd6913b334e04754f99a36
Martin Grandrath <mail@martin-grandrath.de>
2022-02-27 16:24:51 +0100

    Add db columns for seasons

---
+++ server/prisma/migrations/20220227152414_add_seasons/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Recipe" ADD COLUMN "seasonsFall" BOOLEAN;
+ALTER TABLE "Recipe" ADD COLUMN "seasonsSpring" BOOLEAN;
+ALTER TABLE "Recipe" ADD COLUMN "seasonsSummer" BOOLEAN;
+ALTER TABLE "Recipe" ADD COLUMN "seasonsWinter" BOOLEAN;
