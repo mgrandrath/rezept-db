fbd20f9c377d0e308876da943ea04cf86b34338e
Martin Grandrath <mail@martin-grandrath.de>
2022-03-30 16:08:39 +0200

    Opt into concurrent mode of React 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Router } from "./router.js";
 import { ToastContextProvider } from "./toast.js";
 
 const queryClient = new QueryClient();
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-ReactDOM.render(
-  <React.StrictMode>
+root.render(
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ToastContextProvider>
         <Router />
       </ToastContextProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
