d542a30c7ced2249a37fcba6f9baede9b9ac5a7d
Martin Grandrath <mail@martin-grandrath.de>
2022-04-03 14:58:24 +0200

    Simplify storing state in query by using JSON encoding

--- client/src/util/react.js
+++ client/src/util/react.js
@@ -1,60 +1,14 @@
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
-const ARRAY_MARKER = "[]";
-
-const addArrayMarker = (key) => `${key}${ARRAY_MARKER}`;
-const removeArrayMarker = (key) =>
-  key.substring(0, key.length - ARRAY_MARKER.length);
-const hasArrayMarker = (key) => key.endsWith(ARRAY_MARKER);
-
-const searchParamsToObject = (urlSearchParams) =>
-  [...urlSearchParams.entries()].reduce((entries, [key, value]) => {
-    if (value === "true" || value === "false") {
-      value = JSON.parse(value);
-    }
-
-    if (hasArrayMarker(key)) {
-      return {
-        ...entries,
-        [removeArrayMarker(key)]: (
-          entries[removeArrayMarker(key)] ?? []
-        ).concat([value]),
-      };
-    }
-
-    const nestedKey = /^(.*)\[(.*)\]$/.exec(key);
-    if (nestedKey) {
-      return {
-        ...entries,
-        [nestedKey[1]]: {
-          ...entries[nestedKey[1]],
-          [nestedKey[2]]: value,
-        },
-      };
-    }
-
-    return {
-      ...entries,
-      [key]: value,
-    };
-  }, {});
-
 const objectToSearchParams = (object) =>
   Object.fromEntries(
-    Object.entries(object).flatMap(([key, value]) => {
-      if (Array.isArray(value)) {
-        return [[addArrayMarker(key), value]];
-      }
-      if (value && typeof value === "object") {
-        return Object.entries(value).map(([innerKey, innerValue]) => [
-          `${key}[${innerKey}]`,
-          innerValue,
-        ]);
-      }
+    Object.entries(object).map(([key, value]) => [key, JSON.stringify(value)])
+  );
 
-      return [[key, value]];
-    })
+const searchParamsToObject = (searchParams) =>
+  Object.fromEntries(
+    [...searchParams.entries()].map(([key, value]) => [key, JSON.parse(value)])
   );
 
 export const useUrlState = (defaultValues) => {
