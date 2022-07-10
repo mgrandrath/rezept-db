7c7ff30bd68030ba941afa5416e9cd4fcec333f9
Martin Grandrath <mail@martin-grandrath.de>
2022-07-10 15:25:15 +0200

    Convert router to TS

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { Router } from "./router.js";
+import { Router } from "./router";
 import { ToastContextProvider } from "./toast.js";
 
 const queryClient = new QueryClient();
