6fd4fe8297fae387960acc44593c45d2f90e2a03
Martin Grandrath <mail@martin-grandrath.de>
2022-07-15 15:21:33 +0200

    Fix code formatting

--- client/src/util/object.ts
+++ client/src/util/object.ts
@@ -1,11 +1,16 @@
 import deepmergeLib from "deepmerge";
 
-const overrideArrayMerge = (destinationArray: object[], sourceArray: object[]) => sourceArray;
-export const deepmerge = (...objects: ReadonlyArray<object | undefined | null>) => {
+const overrideArrayMerge = (
+  destinationArray: object[],
+  sourceArray: object[]
+) => sourceArray;
+export const deepmerge = (
+  ...objects: ReadonlyArray<object | undefined | null>
+) => {
   return deepmergeLib.all(
     // Removing non-objects by filtering would be preferable but unfortunately
     // leads to a TypeError when running tests with Jest (v27.5.1).
-    objects.map(object => object ?? {}),
+    objects.map((object) => object ?? {}),
     { arrayMerge: overrideArrayMerge }
   );
 };
--- client/src/util/url.ts
+++ client/src/util/url.ts
@@ -1,6 +1,9 @@
 import { generatePath } from "react-router-dom";
 
-export const urlPath = (parts: TemplateStringsArray, ...params: ReadonlyArray<string>) =>
+export const urlPath = (
+  parts: TemplateStringsArray,
+  ...params: ReadonlyArray<string>
+) =>
   params
     .map(encodeURIComponent)
     .reduce((path, param, index) => path + param + parts[index + 1], parts[0]);
