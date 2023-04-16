73280af0d24dc526493cfb8face642a11b466257
Martin Grandrath <mail@martin-grandrath.de>
2023-04-16 14:46:12 +0200

    Highlight active nav bar item with blue background to make it more visible

--- client/src/components/screen.tsx
+++ client/src/components/screen.tsx
@@ -9,6 +9,7 @@ import {
   PlusCircle as AddIcon,
 } from "bootstrap-icons-react";
 import { addRecipeRoute, recipesRoute } from "../routes";
+import classNames from "classnames";
 
 interface NavLinkProps extends BsNavLinkProps {
   to: To;
@@ -29,30 +30,36 @@ const NavLink = (props: NavLinkProps) => {
       currentPathname.startsWith(destinationPathname) &&
       currentPathname.charAt(destinationPathname.length) === "/");
 
-  return <Nav.Link {...linkProps} active={isActive} as={Link} to={to} />;
+  const className = classNames([
+    "ms-4",
+    "py-1",
+    { "text-bg-primary": isActive },
+  ]);
+
+  return (
+    <Nav.Item className={className}>
+      <Nav.Link {...linkProps} active={isActive} as={Link} to={to} />
+    </Nav.Item>
+  );
 };
 
 export const AppHeader = () => {
   return (
-    <Navbar bg="dark" variant="dark" className="px-4">
+    <Navbar bg="dark" variant="dark" className="px-4 py-0">
       <Navbar.Brand>
         <Link to="/" className="text-reset text-decoration-none">
           Rezept-DB
         </Link>
       </Navbar.Brand>
       <Nav className="ms-auto">
-        <Nav.Item className="ms-4">
-          <NavLink to={recipesRoute.url({})}>
-            <FindIcon className="d-block mx-auto mb-1" width={18} height={18} />
-            Find recipe
-          </NavLink>
-        </Nav.Item>
-        <Nav.Item className="ms-4">
-          <NavLink to={addRecipeRoute.url({})}>
-            <AddIcon className="d-block mx-auto mb-1" width={18} height={18} />
-            Add recipe
-          </NavLink>
-        </Nav.Item>
+        <NavLink to={recipesRoute.url({})}>
+          <FindIcon className="d-block mx-auto mb-1" width={18} height={18} />
+          Find recipe
+        </NavLink>
+        <NavLink to={addRecipeRoute.url({})}>
+          <AddIcon className="d-block mx-auto mb-1" width={18} height={18} />
+          Add recipe
+        </NavLink>
       </Nav>
     </Navbar>
   );
